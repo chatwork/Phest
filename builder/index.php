b622f7ce32a89b1a45e98b16d18535f409be2513
Masaki Yamamoto <masaki@chatwork.co.jp>
2013-11-18 15:17:43 +0900

    バージョン番号更新

--- builder/index.php
+++ builder/index.php
@@ -14,7 +14,7 @@
 	define('DIR_BUILDER',dirname(__FILE__));
 	require(DIR_BUILDER.'/config.php');
 	
-	$ver = 'v0.4.4b';
+	$ver = 'v0.4.5b';
 	
 	error_reporting(E_ALL);
 	ini_set('display_errors','On');
